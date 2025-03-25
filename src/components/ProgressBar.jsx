d94cdaf7b720558389c1c28344a20cf87db6c8d8
Ashwin Menghar <ashwinmenghar123@gmail.com>
2025-03-25 21:47:34 +0530

    Refactor ProgressBar and Step components for improved readability and performance

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 
 const ProgressBar = ({ step }) => {
+  const progressWidth = Math.min((step / 3) * 100, 100);
+  const progressBarClass = step > 0 && step <= 4 ? "bg-green-700" : "";
+
   return (
     <div
-      className={`progress-bar h-full ${
-        step > 0 && step <= 4 ? "bg-green-700" : ""
-      }`}
-      style={{ width: `${(step / 3) * 100}%` }}
+      className={`progress-bar h-full ${progressBarClass}`}
+      style={{ width: `${progressWidth}%` }}
     ></div>
   );
 };
--- src/components/Step.jsx
+++ src/components/Step.jsx
@@ -4,30 +4,29 @@ import { steps } from "../data";
 const Step = ({ step, isCompleted }) => {
   return (
     <>
-      {steps.map((stepName, index) => (
-        <div
-          className="flex flex-col justify-center items-center flex-1"
-          key={stepName}
-        >
+      {steps.map((stepName, index) => {
+        const isActive = step >= index + 1 || isCompleted;
+        const isCurrent = step === index;
+        const stepClass = isActive
+          ? "bg-green-400 text-white"
+          : isCurrent
+          ? "bg-blue-700 text-white"
+          : "bg-gray-300 text-black";
+
+        return (
           <div
-            className={`border-2 border-black
-                      ${
-                        step >= index + 1 || isCompleted
-                          ? "bg-green-400 text-white"
-                          : step == index
-                          ? "bg-blue-700 text-white"
-                          : "bg-gray-300 text-black"
-                      } h-10 w-10 rounded-full flex justify-center items-center cursor-pointer`}
+            className="flex flex-col justify-center items-center flex-1"
+            key={stepName}
           >
-            {step >= index + 1 || isCompleted ? (
-              <span>&#10003;</span>
-            ) : (
-              index + 1
-            )}
+            <div
+              className={`border-2 border-black ${stepClass} h-10 w-10 rounded-full flex justify-center items-center cursor-pointer`}
+            >
+              {isActive ? <span>&#10003;</span> : index + 1}
+            </div>
+            <p>{stepName}</p>
           </div>
-          <p>{stepName}</p>
-        </div>
-      ))}
+        );
+      })}
     </>
   );
 };
